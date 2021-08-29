02b67f9c84237179f73f83f94db4df0246e8c069
Otavio Santana <otaviopolianasantana@gmail.com>
2021-08-29 09:33:07 +0100

    updates playerdsl

--- src/main/java/org/soujava/PlayerDSL.java
+++ src/main/java/org/soujava/PlayerDSL.java
@@ -4,13 +4,14 @@ import javax.money.MonetaryAmount;
 import java.time.Year;
 import java.util.Objects;
 
-public class PlayerDSL implements FluentPlayer.PlayerStart,
+public class PlayerDSL implements FluentPlayer,
         FluentPlayer.PlayerEnd, FluentPlayer.PlayerEmail, FluentPlayer.PlayerPosition,
-        FluentPlayer.PlayerSalary, FluentPlayer {
+        FluentPlayer.PlayerSalary {
 
     static final Year SOCCER_BORN = Year.of(1863);
 
-    PlayerDSL() {
+    PlayerDSL(String name) {
+        this.name = name;
     }
 
     private String name;
@@ -25,61 +26,34 @@ public class PlayerDSL implements FluentPlayer.PlayerStart,
 
     private MonetaryAmount salary;
 
+
     @Override
-    public PlayerStart name(String name) {
-        Objects.requireNonNull(name, "name is required");
-        return this;
+    public PlayerEnd start(Year start) {
+        return null;
     }
 
     @Override
-    public FluentPlayer.PlayerEnd start(Year start) {
-        Objects.requireNonNull(start, "start is required");
-        if (Year.now().isBefore(start)) {
-            throw new IllegalArgumentException("you cannot start in the future");
-        }
-        if (SOCCER_BORN.isAfter(start)) {
-            throw new IllegalArgumentException("Soccer was not born on this time");
-        }
-        this.start = start;
-        return this;
+    public PlayerPosition end(Year start) {
+        return null;
     }
 
     @Override
-    public FluentPlayer.PlayerEmail end(Year end) {
-        Objects.requireNonNull(end, "end is required");
-
-        if (start != null && start.isAfter(end)) {
-            throw new IllegalArgumentException("the last year of a player must be equal or higher than the start.");
-        }
-
-        if (SOCCER_BORN.isAfter(end)) {
-            throw new IllegalArgumentException("Soccer was not born on this time");
-        }
-        this.end = end;
-        return this;
+    public PlayerSalary position(Position position) {
+        return null;
     }
 
     @Override
-    public FluentPlayer.PlayerPosition email(Email email) {
-        this.email = Objects.requireNonNull(email, "email is required");
-        return this;
+    public PlayerEmail salary(MonetaryAmount salary) {
+        return null;
     }
 
     @Override
-    public FluentPlayer.PlayerSalary position(Position position) {
-        this.position = Objects.requireNonNull(position, "position is required");
-        return this;
+    public Player email(Email email) {
+        return null;
     }
 
     @Override
-    public Player salary(MonetaryAmount salary) {
-        Objects.requireNonNull(salary, "salary is required");
-        if (salary.isNegativeOrZero()) {
-            throw new IllegalArgumentException("A player needs to earn money to play; otherwise, it is illegal.");
-        }
-        this.salary = salary;
-        return new Player(name, start, end, email, position, salary);
+    public Player build() {
+        return null;
     }
-
-
 }
