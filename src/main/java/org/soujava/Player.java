533ae1327cbb4629493f9e785c296ce4e12e0bae
Otavio Santana <otaviopolianasantana@gmail.com>
2021-08-29 09:01:51 +0100

    removes bean validation at Player class

--- src/main/java/org/soujava/Player.java
+++ src/main/java/org/soujava/Player.java
@@ -1,10 +1,6 @@
 package org.soujava;
 
 import javax.money.MonetaryAmount;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.PositiveOrZero;
 import java.time.Year;
 import java.util.Optional;
 
@@ -12,26 +8,18 @@ public class Player {
 
     static final Year SOCCER_BORN = Year.of(1863);
 
-    @NotBlank
     private String name;
 
-    @NotNull
-    @PastOrPresent
     private Year start;
 
-    @PastOrPresent
     private Year end;
 
-    @NotNull
     private Email email;
 
-    @NotNull
     private Position position;
 
-    @NotNull
     private MonetaryAmount salary;
 
-    @PositiveOrZero
     private int goal = 0;
 
     Player(String name, Year start, Year end, Email email, Position position, MonetaryAmount salary) {
