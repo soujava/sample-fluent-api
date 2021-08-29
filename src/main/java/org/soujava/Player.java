0b7f4a05a15ff983f705236f4d2641c0c4d8f7fe
Otavio Santana <otaviopolianasantana@gmail.com>
2021-08-29 08:57:29 +0100

    removes builder

--- src/main/java/org/soujava/Player.java
+++ src/main/java/org/soujava/Player.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.PositiveOrZero;
 import java.time.Year;
-import java.util.Objects;
 import java.util.Optional;
 
 public class Player {
@@ -87,105 +86,5 @@ public class Player {
         this.end = end;
     }
 
-    public PlayerBuilder toBuilder() {
-        return new PlayerBuilder(this);
-    }
 
-    public static PlayerBuilder builder() {
-        return new PlayerBuilder();
-    }
-
-    public static FluentPlayer.PlayerStart name(String name) {
-        FluentPlayer dsl = new PlayerDSL();
-        return dsl.name(name);
-    }
-
-    public static class PlayerBuilder {
-
-        private String name;
-
-        private Year start;
-
-        private Year end;
-
-        private Email email;
-
-        private Position position;
-
-        private MonetaryAmount salary;
-
-        private PlayerBuilder() {
-        }
-
-        private PlayerBuilder(Player player) {
-            this.name = player.name;
-            this.start = player.start;
-            this.end = player.end;
-            this.email = player.email;
-            this.position = player.position;
-            this.salary = player.salary;
-        }
-
-        public PlayerBuilder withName(String name) {
-            this.name = Objects.requireNonNull(name, "name is required");
-            return this;
-        }
-
-        public PlayerBuilder withStart(Year start) {
-            Objects.requireNonNull(start, "start is required");
-            if (Year.now().isBefore(start)) {
-                throw new IllegalArgumentException("you cannot start in the future");
-            }
-            if (SOCCER_BORN.isAfter(start)) {
-                throw new IllegalArgumentException("Soccer was not born on this time");
-            }
-            this.start = start;
-            return this;
-        }
-
-        public PlayerBuilder withEnd(Year end) {
-            Objects.requireNonNull(end, "end is required");
-
-            if (start != null && start.isAfter(end)) {
-                throw new IllegalArgumentException("the last year of a player must be equal or higher than the start.");
-            }
-
-            if (SOCCER_BORN.isAfter(end)) {
-                throw new IllegalArgumentException("Soccer was not born on this time");
-            }
-            this.end = end;
-            return this;
-        }
-
-        public PlayerBuilder withEmail(Email email) {
-            this.email = Objects.requireNonNull(email, "email is required");
-            return this;
-        }
-
-        public PlayerBuilder withPosition(Position position) {
-            this.position = Objects.requireNonNull(position, "position is required");
-            return this;
-        }
-
-        public PlayerBuilder withSalary(MonetaryAmount salary) {
-            Objects.requireNonNull(salary, "salary is required");
-            if (salary.isNegativeOrZero()) {
-                throw new IllegalArgumentException("A player needs to earn money to play; otherwise, it is illegal.");
-            }
-            this.salary = salary;
-            return this;
-        }
-
-        public Player build() {
-            Objects.requireNonNull(name, "name is required");
-            Objects.requireNonNull(start, "start is required");
-            Objects.requireNonNull(email, "email is required");
-            Objects.requireNonNull(position, "position is required");
-            Objects.requireNonNull(salary, "salary is required");
-
-            return new Player(name, start, end, email, position, salary);
-        }
-
-
-    }
 }
