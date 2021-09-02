7b954fa539e34172bd7f87c2c95cfd8d4d0af3f3
Otavio Santana <otaviopolianasantana@gmail.com>
2021-09-02 13:23:34 +0100

    defines order to test

--- src/test/java/org/soujava/sandwich/OrderTest.java
+++ src/test/java/org/soujava/sandwich/OrderTest.java
@@ -1,8 +1,25 @@
 package org.soujava.sandwich;
 
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import java.util.Locale;
+import java.util.Optional;
+
 public class OrderTest {
+
+    private CurrencyUnit currency;
+
+    @BeforeEach
+    public void setUp() {
+        this.currency = Monetary.getCurrency(Locale.US);
+    }
+    //validation invalid quantity
+    //validation invalid quantity drinks
 
     @Test
     public void shouldCreateAMeatOrderNoBeverages() {
@@ -12,23 +29,54 @@ public class OrderTest {
                 .meat()
                 .quantity(2)
                 .noBeveragesThanks();
+
+        var drink = checkout.getDrink();
+        Assertions.assertTrue(drink.isEmpty());
+        Assertions.assertEquals(0, checkout.getDrinkQuantity());
+        Sandwich sandwich = checkout.getSandwich();
+        Assertions.assertEquals(Bread.GLUTEN_FREE, sandwich.getBread());
+        Assertions.assertEquals(Size.LARGE, sandwich.getSize());
+        Assertions.assertEquals(SandwichStyle.MEAT, sandwich.getStyle());
+        Assertions.assertEquals(Money.of(25, currency), sandwich.getPrice());
+        Assertions.assertEquals(sandwich.getPrice().multiply(2L), checkout.getTotal());
     }
 
     @Test
     public void shouldCreateAVeganOrderNoBeverages() {
-        Checkout checkout = Order.bread(Bread.GLUTEN_FREE)
-                .size(Size.LARGE)
+        Checkout checkout = Order.bread(Bread.ITALIAN)
+                .size(Size.MEDIUM)
                 .meat()
-                .quantity(2)
+                .quantity(1)
                 .noBeveragesThanks();
+
+        var drink = checkout.getDrink();
+        Assertions.assertTrue(drink.isEmpty());
+        Assertions.assertEquals(0, checkout.getDrinkQuantity());
+        Sandwich sandwich = checkout.getSandwich();
+        Assertions.assertEquals(Bread.ITALIAN, sandwich.getBread());
+        Assertions.assertEquals(Size.MEDIUM, sandwich.getSize());
+        Assertions.assertEquals(SandwichStyle.MEAT, sandwich.getStyle());
+        Assertions.assertEquals(Money.of(19, currency), sandwich.getPrice());
+        Assertions.assertEquals(sandwich.getPrice(), checkout.getTotal());
     }
 
     @Test
-    public void shouldCreateAnOrder() {
-        Checkout checkout = Order.bread(Bread.GLUTEN_FREE)
-                .size(Size.LARGE)
+    public void shouldCreateAnOrderWithDrink() {
+        Checkout checkout = Order.bread(Bread.PLAIN)
+                .size(Size.SMALL)
                 .meat()
                 .quantity(2)
-                .noBeveragesThanks();
+                .softDrink(2);
+
+        var drink = checkout.getDrink();
+        Assertions.assertFalse(drink.isEmpty());
+        Assertions.assertEquals(2, checkout.getDrinkQuantity());
+        Sandwich sandwich = checkout.getSandwich();
+        Assertions.assertEquals(Bread.PLAIN, sandwich.getBread());
+        Assertions.assertEquals(Size.SMALL, sandwich.getSize());
+        Assertions.assertEquals(SandwichStyle.MEAT, sandwich.getStyle());
+        Assertions.assertEquals(Money.of(14, currency), sandwich.getPrice());
+        Assertions.assertEquals(Money.of(1, currency), drink.get().getPrice());
+        Assertions.assertEquals(Money.of(30, currency), checkout.getTotal());
     }
 }
