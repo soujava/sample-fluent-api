1875e00209058ead796a4dcb0037898620fec113
Otavio Santana <otaviopolianasantana@gmail.com>
2021-09-02 11:03:51 +0100

    creates a checkout entity

--- src/main/java/org/soujava/sandwich/Checkout.java
+++ src/main/java/org/soujava/sandwich/Checkout.java
@@ -1,2 +1,37 @@
-package org.soujava.sandwich;public class Checkout {
+package org.soujava.sandwich;
+
+import javax.money.MonetaryAmount;
+
+public class Checkout {
+
+    private final Sandwich sandwich;
+
+    private final int quantity;
+
+    private final Drink drink;
+
+    private final MonetaryAmount total;
+
+    Checkout(Sandwich sandwich, int quantity, Drink drink, MonetaryAmount total) {
+        this.sandwich = sandwich;
+        this.quantity = quantity;
+        this.drink = drink;
+        this.total = total;
+    }
+
+    public Sandwich getSandwich() {
+        return sandwich;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    public MonetaryAmount getTotal() {
+        return total;
+    }
 }
