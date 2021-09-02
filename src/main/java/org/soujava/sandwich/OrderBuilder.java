0f2e99d168d0edf69acd3821fe5fbb1e2938f6d2
Otavio Santana <otaviopolianasantana@gmail.com>
2021-09-02 11:31:36 +0100

    updates order builder

--- src/main/java/org/soujava/sandwich/OrderBuilder.java
+++ src/main/java/org/soujava/sandwich/OrderBuilder.java
@@ -1,2 +1,40 @@
-package org.soujava.sandwich;public class OrderBuilder {
+package org.soujava.sandwich;
+
+public class OrderBuilder implements Order.SizeOrder, Order.StyleOrder, Order.StyleQuantityOrder, Order.DrinksOrder,
+        Order.DrinksCocktailOrder, Order.DrinksSoftDrinkOrder {
+
+    @Override
+    public Order.StyleOrder size(Size size) {
+        return null;
+    }
+
+    @Override
+    public Order.StyleQuantityOrder vegan() {
+        return null;
+    }
+
+    @Override
+    public Order.StyleQuantityOrder meat() {
+        return null;
+    }
+
+    @Override
+    public Order.DrinksOrder quantity(int quantity) {
+        return null;
+    }
+
+    @Override
+    public Order.DrinksCocktailOrder softDrink(int quantity) {
+        return null;
+    }
+
+    @Override
+    public Order.DrinksSoftDrinkOrder cocktail(int quantity) {
+        return null;
+    }
+
+    @Override
+    public Checkout noBeveragesThanks() {
+        return null;
+    }
 }
